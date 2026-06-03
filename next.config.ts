b7c007cf1013993bbf09626a6528391ef48ebdda
Artiston2005 <ashwinyadav2408@gmail.com>
2026-06-03 13:09:58 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['10.36.90.30'],
+  experimental: {
+    turbopack: {
+      root: __dirname,
+    }
+  }
 };
 
 export default nextConfig;
